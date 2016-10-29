695025b3dad587120fff1a592bcdd3982464bb46
ozarkp17 <ozarkp17@gmail.com>
2016-10-29 14:36:36 -0400

    Add files via upload
    Script to extract GPS coordinates from tweets

---
+++ extract.py
@@ -0,0 +1,6 @@
+import twitter
+
+twitter.api.Api(consumer_key = "https://apps.twitter.com/app/13034455", 
+	consumer_secret = "fwgolbpwaSPRo7yCcUvnpph0Bryg3FTKiGGsM8f72knATwToKC", 
+	access_token_key = "2683524229-A76OJz1zlNO6j8GFhoCxl9Pehd9sCjOe0IZHVXZ", 
+	access_token_secret = "ukrQxOi45xlxOa2qzuYWknTtPQrrqbnscKvPDWDKFkbs0")
